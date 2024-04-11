deb1035c6a36c4279468aeefc5aa5d0db84e5262
Leka Uïla <liam.flandrinck.58@gmail.com>
2024-04-11 15:10:45 +0200

    unused function

--- main.cpp
+++ main.cpp
@@ -6,18 +6,12 @@
 /*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:29:12 by lflandri          #+#    #+#             */
-/*   Updated: 2024/04/09 18:16:00 by lflandri         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:10:23 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-int	numChecker(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (0);
-	return (-1);
-}
 
 int main(int argc, char **argv)
 {
